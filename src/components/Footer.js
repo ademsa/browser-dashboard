96c894c2cb21ab6823cb1013cb634879eb6126fb
Adem <63111861+ademsa@users.noreply.github.com>
2020-11-09 19:42:20 +0100

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,22 @@ import React from 'react';
 export default function Footer() {
     return (
         <React.Fragment>
-            <footer className="footer is-white" id="about">
+            <footer className="footer py-5">
                 <div className="content has-text-centered grey-light">
-                    <div className="tags has-addons my-3 is-block tags-logo">
-                        <span className="tag is-success"><strong className="has-text-white">BROWSER</strong></span>
-                        <span className="tag is-danger has-text-white">Dashboard</span>
-                    </div>
-                    <p className="mt-2">
+                    <p className="has-text-weight-light my-0 is-size-7">
                         Source code available <a href="http://github.com/ademsa/browser-dashboard">here</a> (<a href="https://github.com/ademsa/browser-dashboard/blob/master/LICENSE.md">MIT license</a>).
                     </p>
+                    <p className="has-text-weight-light mt-2 is-size-7">
+                        Developed by
+                    </p>
+                    <a href="https://ademsa.github.io">
+                        <img className="author" src="/browser-dashboard/author.svg" alt="Author" />
+                    </a>
+                    <p className="has-text-weight-light my-2 is-size-7">
+                        Checkout other projects
+                    </p>
+                    <a href="https://ademsa.github.io/regex360" className="mx-2 has-text-weight-normal">RegEx360</a>
+                    <a href="https://ademsa.github.io/web-page-data" className="has-text-weight-normal">Web Page Data</a>
                 </div>
             </footer>
         </React.Fragment>
