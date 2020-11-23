b3cc52d467247f6a1e4e143f10d5ec3dd543034c
Adem <63111861+ademsa@users.noreply.github.com>
2020-11-23 15:10:20 +0100

    v0.1.9

--- precache-manifest.cdf89ec2435ab6d3c513bfa546d0d052.js
+++ precache-manifest.cdf89ec2435ab6d3c513bfa546d0d052.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0b6568732bd7accc5c23247109dedaf0",
+    "revision": "853036f35894ab8b50c60ece1366d570",
     "url": "/browser-dashboard/index.html"
   },
   {
-    "revision": "357446a641fafff8bf52",
-    "url": "/browser-dashboard/static/css/main.ccb2df27.chunk.css"
+    "revision": "b02211ba6b15ebd52b49",
+    "url": "/browser-dashboard/static/css/main.a5f69b8d.chunk.css"
   },
   {
     "revision": "7b538ca599d25e7354d3",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/browser-dashboard/static/js/2.09835aa3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "357446a641fafff8bf52",
-    "url": "/browser-dashboard/static/js/main.bd083533.chunk.js"
+    "revision": "b02211ba6b15ebd52b49",
+    "url": "/browser-dashboard/static/js/main.df87a91a.chunk.js"
   },
   {
     "revision": "2a693038504260686c4f",
