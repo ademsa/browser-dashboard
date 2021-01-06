4b4247810c66e41a909a7dbbf4e2728bcaa6d401
Adem <63111861+ademsa@users.noreply.github.com>
2021-01-06 19:52:49 +0100

    v0.1.14

---
+++ precache-manifest.f1192754850c318dbe302daf07013939.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217b5e959a5be6063303fa8d88f0d9cb",
+    "url": "/browser-dashboard/index.html"
+  },
+  {
+    "revision": "3f81339166d6f2919e42",
+    "url": "/browser-dashboard/static/css/main.0b6c63f2.chunk.css"
+  },
+  {
+    "revision": "ae5fd8dda63915b60135",
+    "url": "/browser-dashboard/static/js/2.ccae9d21.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/browser-dashboard/static/js/2.ccae9d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f81339166d6f2919e42",
+    "url": "/browser-dashboard/static/js/main.7d9cde3c.chunk.js"
+  },
+  {
+    "revision": "2a693038504260686c4f",
+    "url": "/browser-dashboard/static/js/runtime-main.7fe80665.js"
+  }
+]);
