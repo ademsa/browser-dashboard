689d082f9b0eb5c10a7412f5eeeadbbd6a6b0141
Adem <63111861+ademsa@users.noreply.github.com>
2021-02-17 19:20:03 +0100

    Improvements (February 2021)
    * Updated License.md
    * Updated dependencies
    * Removed unused dependencies and scripts
    * Removed setupTests.js
    * Updated footer
    * Updated features

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,14 +8,12 @@ export default function Footer() {
                     <p className="has-text-weight-light my-0 is-size-7">
                         Source code available <a href="http://github.com/ademsa/browser-dashboard">here</a> (<a href="https://github.com/ademsa/browser-dashboard/blob/master/LICENSE.md">MIT license</a>).
                     </p>
-                    <p className="has-text-weight-light mt-2 is-size-7">
-                        Developed by
-                    </p>
-                    <a href="https://ademsa.github.io">
+                    <br />
+                    <a className="mt-2" href="https://ademsabic.com">
                         <img className="author" src="/browser-dashboard/author.svg" alt="Author" />
                     </a>
-                    <p className="has-text-weight-light my-2 is-size-7">
-                        Other projects
+                    <p className="has-text-weight-light mt-2 mb-0 is-size-7">
+                        Explore other projects
                     </p>
                     <a href="https://ademsa.github.io/regex360" className="mx-2 has-text-weight-normal">RegEx360</a>
                     <a href="https://ademsa.github.io/web-page-data" className="has-text-weight-normal">Web Page Data</a>
