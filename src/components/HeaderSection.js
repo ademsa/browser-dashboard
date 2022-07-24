3437747d63d5d7dce5a279a3ee75a734a0e0147b
Adem <63111861+ademsa@users.noreply.github.com>
2022-07-24 18:58:23 +0200

    Improved responsiveness

--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -1,34 +1,55 @@
-import React from 'react';
-import SectionNav from './SectionNav';
+import React from "react";
+import SectionNav from "./SectionNav";
 
 export default function HeaderSection(props) {
-    return (
-        <React.Fragment>
-            <section className="hero is-success is-medium has-text-centered" ref={props.headerRef}>
-                <div className="hero-body">
-                    <div className="container">
-                        <div className="columns is-vcentered">
-                            <div className="column is-offset-1 is-4">
-                                <p className="title is-1">Get to know<br />your browser.</p>
-                                <p className="subtitle is-5">All detected specs, plugins, fonts and features in one place. Sorted and Searchable.</p>
-                            </div>
-                            <div className="column is-offset-1 is-6-tablet is-4-desktop">
-                                <div className="field has-addons">
-                                    <div className="control is-expanded">
-                                        <input className="input is-focused is-rounded is-large is-warning" ref={props.searchTermRef} defaultValue={props.searchTerm} onKeyDown={props.handleSearchByKey} placeholder="Name or ID..." />
-                                    </div>
-                                    <div className="control">
-                                        <button className="button is-warning is-rounded is-large" onClick={props.handleSearchByClick}>Search</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <React.Fragment>
+      <section
+        className="hero is-success is-medium has-text-centered"
+        ref={props.headerRef}
+      >
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-vcentered is-dektop">
+              <div className="column is-offset-1-desktop is-4-desktop">
+                <p className="title is-1">
+                  Get to know
+                  <br />
+                  your browser.
+                </p>
+                <p className="subtitle is-5">
+                  All detected specs, plugins, fonts and features in one place.
+                  Sorted and Searchable.
+                </p>
+              </div>
+              <div className="column is-offset-1-desktop is-6-desktop">
+                <div className="field has-addons">
+                  <div className="control is-expanded">
+                    <input
+                      className="input is-focused is-rounded is-large is-warning"
+                      ref={props.searchTermRef}
+                      defaultValue={props.searchTerm}
+                      onKeyDown={props.handleSearchByKey}
+                      placeholder="Name or ID..."
+                    />
+                  </div>
+                  <div className="control">
+                    <button
+                      className="button is-warning is-rounded is-large"
+                      onClick={props.handleSearchByClick}
+                    >
+                      Search
+                    </button>
+                  </div>
                 </div>
-                <div className="hero-foot">
-                    <SectionNav navType="header" />
-                </div>
-            </section>
-        </React.Fragment>
-    );
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="hero-foot">
+          <SectionNav navType="header" />
+        </div>
+      </section>
+    </React.Fragment>
+  );
 }
--- src/components/SectionNav.js
+++ src/components/SectionNav.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React from "react";
 
 export default function SectionNav(props) {
-    function scrollToSection(event) {
-        event.preventDefault();
-        let navId = props.navType === "bottom" ? "bottom-menu" : "header-menu";
-        Array.from(document.getElementById(navId).children).forEach(element => {
-            element.className = "navbar-item";
-        });
-        event.currentTarget.className += " is-active";
-        document.getElementById(event.currentTarget.innerText.toLowerCase()).scrollIntoView({ "behavior": "smooth", "block": "start" });
-    }
+  function scrollToSection(event) {
+    event.preventDefault();
+    let navId = props.navType === "bottom" ? "bottom-menu" : "header-menu";
+    Array.from(document.getElementById(navId).children).forEach((element) => {
+      element.className = "navbar-item";
+    });
+    event.currentTarget.className += " is-active";
+    document
+      .getElementById(event.currentTarget.innerText.toLowerCase())
+      .scrollIntoView({ behavior: "smooth", block: "start" });
+  }
 
-    return (
-        <React.Fragment>
-            <nav className="navbar is-spaced navbar-items-centered">
-                <div className="navbar-menu is-active" id={props.navType === "bottom" ? "bottom-menu" : "header-menu"}>
-                    <a className="navbar-item" href="#specs" onClick={scrollToSection}>Specs</a>
-                    <a className="navbar-item" href="#plugins" onClick={scrollToSection}>Plugins</a>
-                    <a className="navbar-item" href="#features" onClick={scrollToSection}>Fonts</a>
-                    <a className="navbar-item" href="#fonts" onClick={scrollToSection}>Features</a>
-                    {props.topLinkVisible &&
-                        <a className="navbar-item" href="#top" onClick={scrollToSection}>TOP</a>
-                    }
-                </div>
-            </nav>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <nav className="navbar is-spaced navbar-items-centered">
+        <div
+          className="navbar-menu is-active"
+          id={props.navType === "bottom" ? "bottom-menu" : "header-menu"}
+        >
+          <a className="navbar-item" href="#specs" onClick={scrollToSection}>
+            Specs
+          </a>
+          <a className="navbar-item" href="#plugins" onClick={scrollToSection}>
+            Plugins
+          </a>
+          <a className="navbar-item" href="#features" onClick={scrollToSection}>
+            Fonts
+          </a>
+          <a className="navbar-item" href="#fonts" onClick={scrollToSection}>
+            Features
+          </a>
+          {props.topLinkVisible && (
+            <a className="navbar-item" href="#top" onClick={scrollToSection}>
+              TOP
+            </a>
+          )}
+        </div>
+      </nav>
+    </React.Fragment>
+  );
 }
